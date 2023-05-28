38bd2c3adfd6f546916689fc3e1acc89ed49eaf0
Adrian Estevez <42591423+estevezadrian95@users.noreply.github.com>
2023-05-28 12:58:55 -0300

    Optimizations
    . load models before start server
    . load image without read/write on disk
    . delete image
    . update package

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const fs = require('fs');
 const canvas = require('canvas');
 const faceapi = require('face-api.js');
+
 const { Canvas, Image, ImageData } = canvas;
+const jimp = require('jimp');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -19,28 +20,36 @@ const faceExpressionNet = faceapi.nets.faceExpressionNet;
 Promise.all([
   emotionModel.loadFromDisk(MODEL_URL),
   faceExpressionNet.loadFromDisk(MODEL_URL),
-]).then(startServer);
+])
+  .then(() => {
+    console.log('Modelos cargados. El servidor está listo.');
+    // Iniciar el servidor después de cargar los modelos
+    startServer();
+  })
+  .catch((error) => {
+    console.error('Error al cargar los modelos:', error);
+  });
 
 function startServer() {
-  console.log('Modelos cargados. El servidor está listo.');
 
-  // Ruta de la API
+  app.listen(port, () => {
+    console.log(`El servidor está escuchando en el puerto ${port}`);
+  });
+
   app.post('/detect-emotion', async (req, res) => {
     const base64Data = req.body.image.replace(/^data:image\/png;base64,/, '');
+    const image = new Image();
 
-    fs.writeFileSync('image.png', base64Data, 'base64', function (err) {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error al guardar la imagen.' });
-      }
-    });
-
-    const img = await canvas.loadImage('./image.png');
+    try {
+      const jimpImage = await jimp.read(Buffer.from(base64Data, 'base64'));
+      image.src = await jimpImage.getBase64Async(jimp.MIME_PNG); // Convierto el objeto Jimp a HTMLImageElement para faceapi
+      console.log('Imagen cargada con éxito');
+    } catch (error) {
+      console.error('Error al cargar la imagen:', error);
+    }
 
     // Detección de emociones
-    const detections = await faceapi
-      .detectAllFaces(img)
-      .withFaceExpressions();
+    const detections = await faceapi.detectAllFaces(image).withFaceExpressions();
 
     if (detections.length === 0) {
       res.status(400).json({ error: 'No se detectó ninguna cara en la imagen.' });
@@ -48,9 +57,6 @@ function startServer() {
       const emotions = detections[0].expressions;
       res.json({ emotions });
     }
-  });
-
-  app.listen(port, () => {
-    console.log(`El servidor está escuchando en el puerto ${port}`);
+    
   });
 }
