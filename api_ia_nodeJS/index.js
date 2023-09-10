ed3cb9f11e96b38a4880aa8dfd3145e72e0a8e3e
Adrian Estevez <42591423+estevezadrian95@users.noreply.github.com>
2023-09-10 14:30:33 -0300

    detect-emotion v2
    Se realizaron modificaciones para que reciba en la request x cantidad de imagenes y procese todas de una, las imagenes van por archivo y no en base64 como se hacia antes

--- api_ia_nodeJS/index.js
+++ api_ia_nodeJS/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const canvas = require('canvas');
 const faceapi = require('face-api.js');
-
+const multer = require('multer');
 const { Canvas, Image, ImageData } = canvas;
-const jimp = require('jimp');
+
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -11,28 +11,43 @@ const port = process.env.PORT || 3001;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// Configuración de multer para manejar la carga de archivos
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 // Configuración de face-api.js
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 const MODEL_URL = './models';
 const faceExpressionNet = faceapi.nets.faceExpressionNet;
 
+
+// Este es el original pero modificamos las expresiones a las que vamos a usar
+// const emotionTranslations = {
+//   neutral: 'Neutral',
+//   happy: 'Feliz',
+//   sad: 'Triste',
+//   angry: 'Enojado',
+//   fearful: 'Asustado',
+//   disgusted: 'Disgustado',
+//   surprised: 'Sorprendido'
+// };
+
 const emotionTranslations = {
-  neutral: 'Neutral',
-  happy: 'Feliz',
-  sad: 'Triste',
-  angry: 'Enojado',
-  fearful: 'Asustado',
-  disgusted: 'Disgustado',
-  surprised: 'Sorprendido'
+  neutral: 'Calma',
+  happy: 'Alegria',
+  sad: 'Tristeza',
+  angry: 'Enojo',
+  fearful: 'Tristeza',
+  disgusted: 'Tristeza',
+  surprised: 'Sorpresa'
 };
 
 Promise.all([
   faceExpressionNet.loadFromDisk(MODEL_URL),
-  faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL) // Cargar el modelo SsdMobilenetv1 antes de realizar inferencias
+  faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL)
 ])
   .then(() => {
     console.log('Modelos cargados. El servidor está listo.');
-    // Iniciar el servidor después de cargar los modelos
     startServer();
   })
   .catch((error) => {
@@ -43,30 +58,45 @@ function startServer() {
   app.listen(port, () => {
     console.log(`El servidor está escuchando en el puerto ${port}`);
   });
-
-  app.post('/detect-emotion', async (req, res) => {
-    const base64Data = req.body.image.replace(/^data:image\/png;base64,/, '');
-    const image = new Image();
-
-    try {
-      const jimpImage = await jimp.read(Buffer.from(base64Data, 'base64'));
-      image.src = await jimpImage.getBase64Async(jimp.MIME_PNG);
-      console.log('Imagen cargada con éxito');
-    } catch (error) {
-      console.error('Error al cargar la imagen:', error);
-    }
-
-    // Detección de emociones
-    const detections = await faceapi.detectSingleFace(image).withFaceExpressions();
-
-    if (!detections) {
-      res.status(400).json({ error: 'No se detectó ninguna cara en la imagen.' });
-    } else {
-      const emotions = detections.expressions;
-      // Obtengo el valor mas alto y lo traduzco al español
-      const highestEmotion = Object.keys(emotions).reduce((a, b) => emotions[a] > emotions[b] ? a : b);
-      const translatedEmotion = emotionTranslations[highestEmotion] || highestEmotion;
-      res.json({ emotions: [translatedEmotion] });
-    }
-  });
 }
+
+const maxImageCount = 20;
+app.use('/detect-emotion', upload.array('images', maxImageCount));
+app.post('/detect-emotion', async (req, res) => {
+  const images = req.files;
+
+  if (!images || images.length === 0) {
+    return res.status(400).json({ error: 'No se han enviado imágenes' });
+  }
+
+  try {
+    const emotionsResponses = {};
+
+    for (let i = 0; i < images.length; i++) {
+      const image = images[i];
+      const imageBuffer = image.buffer;
+      const imageResult = {};
+
+      const img = new Image();
+      img.src = imageBuffer;
+
+      // Detección de emociones
+      const detections = await faceapi.detectSingleFace(img).withFaceExpressions();
+
+      if (!detections) {
+        imageResult.emotion = 'No se detectó ninguna cara en la imagen.';
+      } else {
+        const emotions = detections.expressions;
+        const highestEmotion = Object.keys(emotions).reduce((a, b) => (emotions[a] > emotions[b] ? a : b));
+        imageResult.emotion = emotionTranslations[highestEmotion] || highestEmotion;
+      }
+
+      emotionsResponses[`emotion_${i + 1}`] = imageResult.emotion;
+    }
+
+    res.json(emotionsResponses);
+  } catch (error) {
+    console.error('Error al cargar o procesar las imágenes:', error);
+    return res.status(500).json({ error: 'Error al procesar las imágenes.' });
+  }
+});
--- api_ia_python/Server.py
+++ api_ia_python/Server.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from keras.models import model_from_json
 from flask import Flask, request, jsonify
-import base64
+from PIL import Image
 from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
@@ -16,31 +16,38 @@ emotion_model.load_weights("model/emotion_model.h5")
 print("Modelo cargado correctamente")
 
 emotion_dict = {0: "Enojo", 1: "Tristeza", 2: "Tristeza", 3: "Alegria", 4: "Calma", 5: "Tristeza", 6: "Sorpresa"}
-# emotion_dict = {0: "Enojado", 1: "Disgustado", 2: "Disgustado", 3: "Feliz", 4: "Neutral", 5: "Triste", 6: "Sorprendido"}
 
 @app.route('/detect-emotion', methods=['POST'])
-def detect_emotion():
-    image_data = request.json['image']
-    image_data = base64.b64decode(image_data)
+def detect_emotions():
+    
+    images = request.files.getlist('images')
+    if not images:
+        return jsonify({'error': 'No se han enviado imágenes'}), 400
 
-    nparr = np.frombuffer(image_data, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    results = {}
+    for index, image_file in enumerate(images):
+        img = Image.open(image_file)
+        gray_img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2GRAY)
+        face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+        faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.3, minNeighbors=5)
 
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.3, minNeighbors=5)
+        if len(faces) == 0:
+            results[f'emotion_{index + 1}'] = 'No se detectó ninguna cara en la imagen.'
+        else:
+            emotions = []
 
-    if len(faces) == 0:
-        return jsonify({'error': 'No se detectó ninguna cara en la imagen.'}), 400
+            for (x, y, w, h) in faces:
+                roi_gray = gray_img[y:y+h, x:x+w]
+                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                emotion_prediction = emotion_model.predict(cropped_img)
+                maxindex = int(np.argmax(emotion_prediction))
+                emotions.append(emotion_dict[maxindex])
+            
+            # Encuentra la emoción más común en la imagen
+            most_common_emotion = max(set(emotions), key=emotions.count)
+            results[f'emotion_{index + 1}'] = most_common_emotion
 
-    emotions = []
-
-    for (x, y, w, h) in faces:
-        roi_gray = gray_img[y:y+h, x:x+w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        emotion_prediction = emotion_model.predict(cropped_img)
-        maxindex = int(np.argmax(emotion_prediction))
-        return jsonify({'emotion': emotion_dict[maxindex]}), 200
+    return jsonify(results), 200
 
 if __name__ == '__main__':
     http_server = WSGIServer(('0.0.0.0', 3001), app)
